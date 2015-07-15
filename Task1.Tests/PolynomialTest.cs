64412687d0c70e29df7552b37d6cab53f597e3b5
helenakolodko <helena.kolodko@gmail.com>
2015-07-15 16:19:24 +0300

    Added some tests.

--- Task1.Tests/PolynomialTest.cs
+++ Task1.Tests/PolynomialTest.cs
@@ -12,12 +12,57 @@ namespace Task1.Tests
         {
             Polynomial p = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 0 });
             Polynomial pp = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 8, 9.11, 5 });
-            p = p.Add(pp);
-            Assert.AreNotSame(p, pp);
+            Polynomial result = p.Add(pp);
+            Assert.AreNotSame(p, result);
+            Assert.AreNotSame(pp, result);
         }
 
         [TestMethod]
-        public void Equals_Null_ReturnsFalsel()
+        public void Add_TwoPolynomials_ReturnsPolynomialWithCorrectValue()
+        {
+            Polynomial p = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 0 });
+            Polynomial pp = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 8, 9.11, 5 });
+            p = p.Add(pp);
+            Assert.AreEqual(44.31, p.GetValue(1));
+        }
+
+        [TestMethod]
+        public void Subtract_TwoPolynomials_ReturnsNewPolynomial()
+        {
+            Polynomial p = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 0 });
+            Polynomial pp = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 8, 9.11, 5 });
+            Polynomial result = p.Subtract(pp);
+            Assert.AreNotSame(p, result);
+            Assert.AreNotSame(pp, result);
+        }
+
+        [TestMethod]
+        public void Subtract_TwoPolynomials_ReturnsPolynomialWithCorrectValue()
+        {
+            Polynomial p = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 0 });
+            Polynomial pp = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 8, 9.11, 5 });
+            p = p.Subtract(pp);
+            Assert.AreEqual(-22.11, p.GetValue(1));
+        }
+
+        [TestMethod]
+        public void Multiply_ByOne_ReturnsNewPolynomial()
+        {
+            Polynomial p = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 0 });
+            Polynomial result = p.Multiply(1);
+            Assert.AreNotSame(p, result);
+        }
+
+        [TestMethod]
+        public void Multiply_ByOne_ReturnsEqualPolynomial()
+        {
+            Polynomial p = new Polynomial(new double[] { 0, 5.6, 5, 0.5, 0, 0, 0 });
+            Polynomial result = p.Multiply(1);
+            Assert.IsTrue(p == result);
+        }
+
+        [TestMethod]
+        public void Equals_Null_ReturnsFalse()
         {
             Polynomial p = new Polynomial(new double[]{0, 5.6, 5, 0.5, 0, 0, 0});
 	        Assert.IsFalse(p.Equals(null));
